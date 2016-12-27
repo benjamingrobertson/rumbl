71e45cfd14f30a9489248fc6eaf50e70adde43f8
Ben Robertson <benjamingrobertson@me.com>
2016-12-27 17:59:33 -0500

    Re-enable the ecto repository and supervisor

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,9 +1,9 @@
 defmodule Rumbl.Repo do
-  # use Ecto.Repo, otp_app: :rumbl
+  use Ecto.Repo, otp_app: :rumbl
   @moduledoc """
   In memory respository.
   """
-  
+
   def all(Rumbl.User) do
       [%Rumbl.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
        %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
